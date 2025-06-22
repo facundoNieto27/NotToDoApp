1c1795d42aca3498c061aa8049fc84be663d4511
Facundo <facundo@la.machine>
2025-06-22 11:40:02 -0300

    Not to do app first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (newNote) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  };
+
+  function deleteItem(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.iTitle}
+          content={note.tContent}
+          onClick={deleteItem}
+        />
+      ))}
+      <Note title="Hacer hoy" content="Ir al gimnasio" />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
